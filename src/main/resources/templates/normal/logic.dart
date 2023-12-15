6f2c4682146314a775a7f1b97e6f176f99a890a8
hbolin <hbolin@163.com>
2023-12-15 15:01:59 +0800

    2.0.2

--- src/main/resources/templates/normal/logic.dart
+++ src/main/resources/templates/normal/logic.dart
@@ -7,7 +7,7 @@ class @nameLogic extends AppBasePageGetxController {
   final @nameState state = @nameState();
 
   @override
-  Future<void> loadData(bool isLoadingCachedData) async {
+  Future<void> loadData(bool isLoadCachedData) async {
     // TODO: implement loadData
     throw UnimplementedError();
     // TODO:如果使用到的page的参数，则需要判断page的参数是否合法，因为当前是支持动态路由的，所以无法向原生那样强制要求参数不为null
